e481d393a06b8b4c942c19f2f8db1017ac762e2f
msyoki <msyokimutua@gmail.com>
2020-11-10 01:05:10 +0300

    add knox as default authentication

--- DRESSUP/settings.py
+++ DRESSUP/settings.py
@@ -103,6 +103,13 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'knox.auth.TokenAuthentication',
+    ]
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
