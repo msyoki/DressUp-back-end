3fe37148220b2c81f114e790496d805f27c16bfc
omarion3698 <omaribinbakarivic@gmail.com>
2020-11-10 19:15:30 +0300

    Products Api

--- dressup/models.py
+++ dressup/models.py
@@ -3,9 +3,12 @@ from django.dispatch import receiver
 from django.urls import reverse
 from django_rest_passwordreset.signals import reset_password_token_created
 from django.core.mail import send_mail
-
-
 from django.contrib.auth.models import User
+
+from modelcluster.fields import ParentalKey
+from wagtail.core.models import Page, Orderable
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, InlinePanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 # Create your models here.
 @receiver(reset_password_token_created)
@@ -24,10 +27,7 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
         [reset_password_token.user.email]
     )
 
-
-
-# Create your models here.
-
+# Profile Api
 class Profile(models.Model):
     profile_pic= models.ImageField(upload_to='profilepic/',default='default.jpeg')
     user=models.OneToOneField(User,on_delete=models.CASCADE)
@@ -42,4 +42,43 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
-    
+
+# Product Api  
+class Product(Page):
+    sku = models.CharField(max_length=255)
+    short_description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    image = models.ForeignKey('wagtailimages.Image', null=True,blank=True, on_delete=models.SET_NULL, related_name='+')
+
+    content_panels = Page.content_panels + [
+        FieldPanel('sku'),
+        FieldPanel('price'),
+        ImageChooserPanel('image'),
+        FieldPanel('short_description'),
+        InlinePanel('custom_fields', label='Custom fields'),
+    ]
+
+        def get_context(self, request):
+          context = super().get_context(request)
+        fields = []
+        for f in self.custom_fields.get_object_list():
+            if f.options:
+                f.options_array = f.options.split('|')
+                fields.append(f)
+            else:
+                fields.append(f)
+
+        context['custom_fields'] = fields
+
+        return context
+
+
+class ProductCustomField(Orderable):
+    product = ParentalKey(Product, on_delete=models.CASCADE, related_name='custom_fields')
+    name = models.CharField(max_length=255)
+    options = models.CharField(max_length=500, null=True, blank=True)
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('options')
+    ]
