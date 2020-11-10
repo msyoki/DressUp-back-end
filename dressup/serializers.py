79b561c388191f2c6a709ed16453765c4369a050
msyoki <msyokimutua@gmail.com>
2020-11-10 17:11:35 +0300

    serialie profile models and link user reg to profile reg

--- dressup/serializers.py
+++ dressup/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import Profile
 
 # User Serializer
 class UserSerializer(serializers.ModelSerializer):
@@ -16,7 +17,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
-
+        profile=Profile.objects.create(user=user,email=user.email,username=user)
         return user
 
 #Change Password
@@ -28,3 +29,10 @@ class ChangePasswordSerializer(serializers.Serializer):
     """
     old_password = serializers.CharField(required=True)
     new_password = serializers.CharField(required=True)
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Profile
+        fields=('username','first_name','last_name','email','phone','location','profile_pic')
+    
