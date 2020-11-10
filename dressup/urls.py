833a7b49031331b95e8bb68b6314c466b9574b9f
msyoki <msyokimutua@gmail.com>
2020-11-10 17:10:28 +0300

    register Profile model to admin

--- dressup/urls.py
+++ dressup/urls.py
@@ -1,13 +1,19 @@
-from .views import RegisterAPI, ChangePasswordView
+from .views import RegisterAPI,LoginAPI,ChangePasswordView,ProfileAPI,ViewProfile
 from knox import views as knox_views
-from .views import LoginAPI
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
+    path('profiles/', ProfileAPI.as_view(), name='profiles'),
+    path('profile/<int:pk>/',views.ViewProfile),
     path('register/', RegisterAPI.as_view(), name='register'),
     path('login/', LoginAPI.as_view(), name='login'),
     path('logout/', knox_views.LogoutView.as_view(), name='logout'),
     path('change-password/', ChangePasswordView.as_view(), name='change-password'),
     path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
