fcafd84e71c0bda563215fb1cfa35216f4bdad05
msyoki <msyokimutua@gmail.com>
2020-11-10 14:47:36 +0300

    delete termapi on password change and reset paths

--- dressup/urls.py
+++ dressup/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('register/', RegisterAPI.as_view(), name='register'),
     path('login/', LoginAPI.as_view(), name='login'),
     path('logout/', knox_views.LogoutView.as_view(), name='logout'),
-    path('api/change-password/', ChangePasswordView.as_view(), name='change-password'),
-    path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
+    path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
 ]
