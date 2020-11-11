51ced96186a05b6d1ce87c2b76b28031c6593161
msyoki <msyokimutua@gmail.com>
2020-11-11 19:10:48 +0300

    update url paths

--- dressup/urls.py
+++ dressup/urls.py
@@ -7,14 +7,14 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('api/profiles/', ProfilesAPI.as_view(), name='profiles'),
-    path('api/profile/<int:pk>/',views.ProfileAPI),
-    path('api/products/', ProductsAPI.as_view(), name='products'),
-    path('api/register/', RegisterAPI.as_view(), name='register'),
-    path('api/login/', LoginAPI.as_view(), name='login'),
-    path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
-    path('api/change-password/', ChangePasswordView.as_view(), name='change-password'),
-    path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+    path('profiles/', ProfilesAPI.as_view(), name='profiles'),
+    path('profile/<int:pk>/',views.ProfileAPI),
+    path('products/', ProductsAPI.as_view(), name='products'),
+    path('register/', RegisterAPI.as_view(), name='register'),
+    path('login/', LoginAPI.as_view(), name='login'),
+    path('logout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
+    path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
