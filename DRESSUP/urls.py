2acede2f8318c80db499d3633bb26089b6877131
msyoki <msyokimutua@gmail.com>
2020-11-11 19:10:48 +0300

    update url paths

--- DRESSUP/urls.py
+++ DRESSUP/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('dressup.urls')),
+    path('', admin.site.urls),
+    path('api/',include('dressup.urls')),
 ]
