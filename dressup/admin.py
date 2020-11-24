c44e6182a848f72a9ead4de919263586f3f738d8
omarion3698 <omaribinbakarivic@gmail.com>
2020-11-25 00:41:02 +0300

    Customize the admin dashboard

--- dressup/admin.py
+++ dressup/admin.py
@@ -1,7 +1,43 @@
 from django.contrib import admin
-from .models import Profile,Product,Post
+from django.contrib.auth.models import Group
+from django.contrib import messages
+from .models import Profile, Product, Post
 
 # Register your models here.
 admin.site.register(Profile)
-admin.site.register(Product)
 admin.site.register(Post)
+
+
+admin.site.unregister(Group)
+
+admin.site.site_header = 'Login to DressUp Seller Dashboard'
+admin.site.site_title = 'Welcome to DressUp Admin Dashboard'
+admin.site.index_title = 'DressUp Portal'
+
+class ProductAdmin(admin.ModelAdmin):
+  list_display = ('name', 'profile', 'category', 'created_on', 'active') 
+  list_filter = ('name', 'created_on', 'category')
+
+  def active(self, obj): 
+    return obj.is_active == 1
+
+  active.boolean = True
+
+  def make_active(modeladmin, request, queryset):
+    queryset.update(is_active = 1) 
+    messages.success(request, "Selected Record(s) Marked as Active Successfully !!") 
+
+  def make_inactive(modeladmin, request, queryset):
+    queryset.update(is_active = 0)
+    messages.success(request, "Selected Record(s) Marked as Inactive Successfully !!") 
+
+  admin.site.add_action(make_active, "Make Active") 
+  admin.site.add_action(make_inactive, "Make Inactive")
+
+  # def has_delete_permission(self, request, obj = None): 
+  #   return False
+
+  # def has_add_permission(self, request): 
+  #   return False
+
+admin.site.register(Product, ProductAdmin)
