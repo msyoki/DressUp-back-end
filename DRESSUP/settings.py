705e0542c9cfcc849913cc6cd906ef29dd107578
omarion3698 <omaribinbakarivic@gmail.com>
2020-11-25 10:45:06 +0300

    Polish

--- DRESSUP/settings.py
+++ DRESSUP/settings.py
@@ -43,7 +43,7 @@ DATABASES['default'].update(db_from_env)
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:4200',
-    'http://localhost:8000',
+    'http://localhost:8080',
 )
 
 CORS_ORIGIN_ALLOW_ALL =  False
